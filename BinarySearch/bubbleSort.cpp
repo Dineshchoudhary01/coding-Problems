17668fb2965f23942a62cbafcad78e8abddf8829
dc967 <dc3879643@gmail.com>
2026-08-15 20:23:23 +0530

    15 aug

---
+++ BinarySearch/bubbleSort.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+  vector<int> arr = {4,1,5,2,3};
+  int n = arr.size();
+
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n-i-1; j++){
+        if(arr[j] > arr[j+1]){
+            swap(arr[j],arr[j+1]);
+        }
+    }
+  }
+
+  for(auto ch : arr){
+    cout << ch << endl;
+  }
+
+
+
+   return 0;
+}
